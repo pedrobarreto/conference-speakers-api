18dc45544852517b111769d82ab6b037e872ee30
Pedro Barreto <pbdeoliveira@gmail.com>
2022-01-14 15:49:54 -0300

    requisito 1

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const fs = require('fs/promises');
+
+function getTalkers() {
+  return fs.readFile('./talker.json', 'utf-8')
+    .then((fileContent) => JSON.parse(fileContent));
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await getTalkers();
+ if (talkers.length === 0) return res.status(HTTP_OK_STATUS).json([]);
+res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {
