62f920edbb796fcd65b0dae5c05f500e5066dfc2
Pedro Barreto <pbdeoliveira@gmail.com>
2022-01-14 21:03:50 -0300

    requisito 4

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_CREATED_STATUS = 201;
 const HTTP_ERROR_STATUS = 404;
 const HTTP_BAD_STATUS = 400;
 const PORT = '3000';
@@ -14,15 +15,19 @@ function generateToken() {
  return crypto.randomBytes(8).toString('hex');
 }
 
-// onde aprendi sobre o modulo email validator do node 
-// https://stackoverflow.com/questions/52456065/how-to-format-and-validate-email-node-js
-const validator = require('email-validator');
-
 const fs = require('fs/promises');
+const middlewares = require('./middlewares');
+
+const { validateAge, validateMail, validateName,
+   validateDate, validateRate, validateAuth } = middlewares;
 
 function getTalkers() {
   return fs.readFile('./talker.json', 'utf-8')
     .then((fileContent) => JSON.parse(fileContent));
+}
+
+function setTalkers(talker) {
+  return fs.writeFile('./talker.json', JSON.stringify(talker));
 }
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -49,18 +54,6 @@ app.get('/talker/:id', async (req, res) => {
   res.status(HTTP_OK_STATUS).json(talkerId);
 });
 
-function validateMail(req, res, next) {
-  const { email } = req.body;
-  if ([email].includes(undefined) || email.length === 0) {
-    return res.status(HTTP_BAD_STATUS).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!validator.validate(email)) {
-    return res.status(HTTP_BAD_STATUS)
-    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  next();
-}
-
 app.post('/login', validateMail, async (req, res) => {
   const { password } = req.body;
   const token = await generateToken();
@@ -73,6 +66,17 @@ app.post('/login', validateMail, async (req, res) => {
     .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   res.status(HTTP_OK_STATUS).json({ token });
+});
+
+app.post('/talker', validateAuth, validateName,
+ validateDate, validateAge, validateRate, async (req, res) => {
+  const talkers = await getTalkers();
+  const { name, age, talk } = req.body;
+
+  talkers.push({ age, id: 5, name, talk });
+  await setTalkers(talkers);
+  res.status(HTTP_CREATED_STATUS).json(talkers[talkers.length - 1]);
+  console.log(talk);
 });
 
 app.listen(PORT, () => {
